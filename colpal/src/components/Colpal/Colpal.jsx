a68e66df513162bfc56201cf08ab508ef1e00d6e
dr-6d <hi.dheerajrao@gmail.com>
2022-01-18 13:07:53 +0530

    second

--- colpal/src/components/Colpal/Colpal.jsx
+++ colpal/src/components/Colpal/Colpal.jsx
@@ -1,31 +1,65 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-bootstrap";
-import { Nav, Container } from "react-bootstrap";
 import "./colpal.css";
+import { masterList } from "./data";
 
-function Colpal() {
-  return (
-    <>
-      <div className="cont">
-        <div className="split-l">
-          <div className="cont">
-            <button id="button">Change</button>
-          </div>
+class Colpal extends Component {
+  constructor() {
+    super();
+    this.state = { index1: 0 };
+  }
+  render() {
+    return (
+      <>
+        <div className="cont">
+          <Split id={2} />
+          <Split id={2} />
+          <Split id={3} />
+          <Split id={3} />
+          <Split id={4} />
+          <Split id={4} />
         </div>
-        <div className="split-m">
-          <div className="cont">
-            <button id="button">Change</button>
-          </div>
-        </div>
-        <div className="split-r">
-          <div className="cont">
-            <button id="button">Change</button>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
+
+class Split extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: Math.floor(Math.random() * masterList.length),
+    };
+    this.id = props.id;
+  }
+
+  render() {
+    return (
+      <>
+        <div
+          style={{ backgroundColor: masterList[this.state.index][this.id] }}
+          className="split"
+        >
+          <div
+            style={{ backgroundColor: masterList[this.state.index][this.id] }}
+            className="cont"
+          >
+            <button
+              onClick={() => {
+                this.setState({
+                  index: Math.floor(Math.random() * masterList.length),
+                });
+              }}
+              className="button-54"
+            >
+              change
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
 export default Colpal;
