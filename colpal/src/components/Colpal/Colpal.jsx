fd89f9f80acca5c2f61fe91c72462bc8c1ebc11c
dr-6d <hi.dheerajrao@gmail.com>
2022-01-18 14:42:53 +0530

    second

--- colpal/src/components/Colpal/Colpal.jsx
+++ colpal/src/components/Colpal/Colpal.jsx
@@ -11,9 +11,9 @@ const Colpal = () => {
   const [value1, setValue1] = React.useState(3);
 
   const [bool1, setBool1] = React.useState(true);
-  const [bool2, setBool2] = React.useState(true);
+  const [bool2, setBool2] = React.useState(false);
   const [bool3, setBool3] = React.useState(true);
-  const [bool4, setBool4] = React.useState(true);
+  const [bool4, setBool4] = React.useState(false);
   const [bool5, setBool5] = React.useState(true);
 
   return (
@@ -80,6 +80,7 @@ class Split extends Component {
     super(props);
     this.state = {
       index: Math.floor(Math.random() * masterList.length),
+      index2: Math.floor(Math.random() * masterList.length),
     };
     this.id = props.id;
   }
@@ -93,18 +94,41 @@ class Split extends Component {
         >
           <div
             style={{ backgroundColor: masterList[this.state.index][this.id] }}
-            className="cont"
+            // className="cont-2"
           >
             <button
+              style={{
+                backgroundColor: masterList[this.state.index2][this.id],
+              }}
               onClick={() => {
                 this.setState({
                   index: Math.floor(Math.random() * masterList.length),
+                  index2: Math.floor(Math.random() * masterList.length),
                 });
               }}
               className="button-54"
             >
               change
             </button>
+            <br />
+            <h6
+              style={{
+                textAlign: "center",
+                marginLeft: "1vw",
+                marginRight: "1vw",
+              }}
+            >
+              <br />
+              <span className="colorl txt">
+                color :<h4>{masterList[this.state.index][0]}</h4>
+              </span>
+            </h6>
+            <br />
+            <h5>
+              <span className="colorLHex txt">
+                hex : <h3>{masterList[this.state.index][this.id]}</h3>
+              </span>
+            </h5>
           </div>
         </div>
       </>
